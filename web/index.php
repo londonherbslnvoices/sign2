e73f6d1bbcaec63e97cfbde846b61a26ba262605
londonherbslnvoices <londonherbslnvoices@gmail.com>
2025-01-16 18:03:43 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,11 +1,11 @@
 <?php
-
 use App\EmailDecoder;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-// Get the current request URI path
-$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-// Pass the path to the EmailDecoder
-EmailDecoder::process($path);
+if (isset($_GET['userid']) && !empty($_GET['userid'])) {
+    $email = $_GET['userid'];
+    EmailDecoder::decode($email);
+} else {
+    echo "No email provided.";
+}
